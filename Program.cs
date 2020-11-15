f45baf7adecf7a244c1f6d540e7bbd4fbc745d77
Annvel236 <veldymanova@gmail.com>
2020-11-15 20:04:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace passwordLength
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int passLength;
+      string passChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      Random random = new Random();
+
+      Console.WriteLine("Enter password length : ");
+      passLength = Convert.ToInt32(Console.ReadLine());
+
+      char[] password = new char[passLength];
+
+      for (int i = 0; i < passLength; i++)
+      {
+        password[i] = passChars[random.Next(passChars.Length)];
+      }
+
+      Console.WriteLine(password);
+    }
+  }
+}
